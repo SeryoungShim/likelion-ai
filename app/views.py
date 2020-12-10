54f2abc911c929fba9cdb081f5a09fcb1adc1c46
jiwon <26162616@naver.com>
2020-12-10 17:45:08 +0900

    temp

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Photo
 
 # Create your views here.
